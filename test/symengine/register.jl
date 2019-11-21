e3f4dc6a101ab4d1bfaca2a1c04f206b132eb182
Roger-luo <hiroger@qq.com>
2019-11-20 20:40:11 -0500

    fix partial_tr

--- test/symengine/register.jl
+++ test/symengine/register.jl
@@ -1,6 +1,11 @@
-using Test, YaoSym
+using Test, YaoSym, YaoBase
 
 @testset "constructors" begin
     @test ket"011" isa SymReg
     @test (ket"011")' isa AdjointSymReg
+end
+
+@testset "partial_tr" begin
+    @vars α β
+    @test α * ket"101" + β * ket"111" |> partial_tr(2:3) == (α + β) * ket"1"
 end
